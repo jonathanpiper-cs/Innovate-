652647cfea70966f7b2a62afa279b3395fc402fc
Jonathan Piper <jonathan.piper@contentstack.com>
2024-03-18 14:23:21 -0700

    init

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,53 +1,47 @@
-import React, { useState, useEffect } from 'react';
-import { onEntryChange } from '../contentstack-sdk';
-import RenderComponents from '../components/render-components';
-import { getPageRes } from '../helper';
-import Skeleton from 'react-loading-skeleton';
-import { Props, Context } from "../typescript/pages";
+import React, { useState, useEffect } from "react"
+import { onEntryChange } from "../contentstack-sdk"
+import RenderComponents from "../components/render-components"
+import { getPageRes } from "../helper"
+import Skeleton from "react-loading-skeleton"
+import { Props, Context } from "../typescript/pages"
 
 export default function Home(props: Props) {
+	const { page, entryUrl } = props
 
-  const { page, entryUrl } = props;
+	const [getEntry, setEntry] = useState(page)
 
-  const [getEntry, setEntry] = useState(page);
+	useEffect(() => {
+		console.log("hi")
 
-  async function fetchData() {
-    try {
-      const entryRes = await getPageRes(entryUrl);
-      if (!entryRes) throw new Error('Status code 404');
-      setEntry(entryRes);
-    } catch (error) {
-      console.error(error);
-    }
-  }
+		async function fetchData() {
+			try {
+				const entryRes = await getPageRes(entryUrl)
+				if (!entryRes) throw new Error("Status code 404")
+				setEntry(entryRes)
+			} catch (error) {
+				console.error(error)
+			}
+		}
+		fetchData()
+	}, [entryUrl])
 
-  useEffect(() => {
-    console.log('hi')
-    onEntryChange(() => fetchData());
-  }, []);
-
-  return getEntry ? (
-    <RenderComponents
-      pageComponents={getEntry.page_components}
-      contentTypeUid='page'
-      entryUid={getEntry.uid}
-      locale={getEntry.locale}
-    />
-  ) : (
-    <Skeleton count={3} height={300} />
-  );
+	return getEntry ? (
+		<RenderComponents pageComponents={getEntry.page_components} contentTypeUid="page" entryUid={getEntry.uid} locale={getEntry.locale} />
+	) : (
+		<Skeleton count={3} height={300} />
+	)
 }
 
 export async function getStaticProps(context: Context) {
-  try {
-    const entryRes = await getPageRes('/');
-    return {
-      props: {
-        entryUrl: '/',
-        page: entryRes,
-      },
-    };
-  } catch (error) {
-    return { notFound: true };
-  }
+	try {
+		const entryRes = await getPageRes("/")
+		return {
+			props: {
+				entryUrl: "/",
+				page: entryRes,
+			},
+		}
+	} catch (error) {
+		return { notFound: true }
+	}
 }
